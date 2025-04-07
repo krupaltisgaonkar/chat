cfe438ae05e24cea267b5eee1009ec2e051e9719
krupaltisgaonkar <krupalt78@gmail.com>
2025-04-07 16:37:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ try {
 let index = 0
 
 if (settings.uid == "FYa5nSQUY7cU3GnBGDrLn61fwsF3" || settings.uid == "sbfj3UM04VXjgZdHq4nALxQXsIh1"){
-    localstorage.setItem("admin", true)
+    localStorage.setItem("admin", true)
     alert("You are an admin! Press control+shift+e to see a prompt open. When you see it, type in 'admin' without the parenthesis. Your name and profile pic will change to being admin.")
 }
 
